951279ff62f7f0a4012bd8084df3d43f8bb20316
Scott <scott@paragonie.com>
2015-04-21 19:13:57 -0400

    Create autoload.php

---
+++ autoload.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Paragon Initiative Enterprises - Password Lock
+ * PSR-4 compatible autoloader
+ */
+\spl_autoload_register(function ($class) {
+    // Project-specific namespace prefix
+    $prefix = 'ParagonIE\\PasswordLock';
+
+    // Base directory for the namespace prefix
+    $base_dir = __DIR__.'/src/';
+
+    // Does the class use the namespace prefix?
+    $len = \strlen($prefix);
+    if (\strncmp($prefix, $class, $len) !== 0) {
+        // no, move to the next registered autoloader
+        return;
+    }
+
+    // Get the relative class name
+    $relative_class = \substr($class, $len);
+
+    // Replace the namespace prefix with the base directory, replace namespace
+    // separators with directory separators in the relative class name, append
+    // with .php
+    $file = $base_dir.
+        \str_replace(
+            ['\\', '_'],
+            '/',
+            $relative_class
+        ).'.php';
+
+    // If the file exists, require it
+    if (\file_exists($file)) {
+        require $file;
+    }
+});
